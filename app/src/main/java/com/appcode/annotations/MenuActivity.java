923a5dc70ad0c5b15232b6890ca66f7df4701678
Magno Ramos <magno.r.dev@gmail.com>
2019-03-28 19:29:39 -0300

    menu configurado

--- app/src/main/java/com/appcode/annotations/MenuActivity.java
+++ app/src/main/java/com/appcode/annotations/MenuActivity.java
@@ -3,38 +3,51 @@ package com.appcode.annotations;
 import android.os.Bundle;
 import android.view.MenuItem;
 
+import com.appcode.annotations.fragments.NotesFragment;
+import com.appcode.annotations.fragments.TasksFragment;
+import com.appcode.annotations.fragments.ThemesFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import butterknife.BindView;
-import butterknife.ButterKnife;
+import androidx.fragment.app.Fragment;
 
 public class MenuActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
-
-    @BindView(R.id.bottom_navigation_view)
-    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
 
-        ButterKnife.bind(this);
-
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation_view);
         bottomNavigationView.setOnNavigationItemSelectedListener(this);
-        if (savedInstanceState == null) {
-            getSupportFragmentManager()
-                    .beginTransaction()
-                    .replace(R.id.fragment_container, new FolderFragment())
-                    .commit();
 
+        if (savedInstanceState == null) {
+            startFragment(new NotesFragment());
         }
     }
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.item_notes:
+                startFragment(new NotesFragment());
+                break;
+            case R.id.item_tasks:
+                startFragment(new TasksFragment());
+                break;
+            case R.id.item_themes:
+                startFragment(new ThemesFragment());
+                break;
+        }
         return true;
+    }
+
+    private void startFragment(Fragment fragment) {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.fragment_container, fragment)
+                .commit();
     }
 }
